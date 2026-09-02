10730b46e1f3026a7b09f0d60f8b2ec470d35596
kosuke kurose <kousuke10010@gmail.com>
2026-09-02 17:08:07 +0900

    SEとBGMが流れるようにAudioManagerの処理を追加

--- Assets/Script/Audio/AudioManager.cs
+++ Assets/Script/Audio/AudioManager.cs
@@ -2,15 +2,53 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static AudioManager Instance{ get; private set; }
+
+    [SerializeField] private AudioSource _bgmSource; // BGM用のAudioSource
+    [SerializeField] private AudioSource _seSource;  // 効果音用のAudioSource
+
+    private void Awake()
     {
-        
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayBGM(AudioClip clip)
     {
-        
+        if(_bgmSource.clip == clip) return; // 既に再生中のBGMと同じ場合は何もしない
+        _bgmSource.clip = clip;
+        _bgmSource.Play();
+    }
+
+    public void StopBGM()
+    {
+        _bgmSource.Stop();
+    }
+
+    public void PlaySE(AudioClip clip)
+    {
+        _seSource.PlayOneShot(clip);
+    }
+
+    public void StopSE()
+    {
+        _seSource.Stop();
+    }
+
+    public void SetBGMVolume(float volume)
+    {
+        _bgmSource.volume = volume;
+    }
+
+    public void SetSEVolume(float volume)
+    {
+        _seSource.volume = volume;
     }
 }
